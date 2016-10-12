f5518e3a824fc81ee3438d5d5c4806b0bb5e4e19
Alan <placebo_al@hotmail.com>
2016-10-12 17:16:01 +1100

    Tried new update for settings production

--- mysite/settings_production.py
+++ mysite/settings_production.py
@@ -1,12 +1,30 @@
 from mysite.settings import *
 import dj_database_url
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    "default": dj_database_url.config(default='postgres://localhost'),
+}
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 ALLOWED_HOSTS = ['*']
 
 DEBUG = False
 
 STATICFILES_STORAGE ='whitenoise.django.GzipManifestStaticFilesStorage'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+        }
+    }
+}
