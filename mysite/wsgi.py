b2b1794a2beb3b228560bd096b9a251a7fe889ce
Alan <placebo_al@hotmail.com>
2016-10-12 16:21:36 +1100

    Add production settings file

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -3,7 +3,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings_production")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
