cf3f3eddfd54e04d0f88a70a31bdc4a8e616b04e
Alan <placebo_al@hotmail.com>
2016-10-12 16:06:16 +1100

    Commit before pushing to heroku

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Page
 
+# set up automated slug creation
+class PageAdmin(admin.ModelAdmin):
+	model = Page
+	list_display = ('author', 'title', 'text',)
+	prepopulated_fields = {'slug': ('title',)}
 
-admin.site.register(Page)
+
+admin.site.register(Page, PageAdmin)
+
+
