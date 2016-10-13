75834fc231efc88731ac80453b185b5aa9fe69b9
Alan <placebo_al@hotmail.com>
2016-10-13 12:19:05 +1100

    Add a base page and a detail page

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.page_list, name='page_list'),
+    url(r'^page/(?P<pk>\d+)/$', views.page_detail, name='page_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Page 
 
@@ -6,3 +6,10 @@ from .models import Page
 def page_list(request):
     pages = Page.objects.filter(created_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/page_list.html', {'pages': pages})
+
+
+def page_detail(request, pk):
+	page = get_object_or_404(Page, pk=pk)
+	return render(request, 'blog/page_detail.html', {'page': page})
+
+
