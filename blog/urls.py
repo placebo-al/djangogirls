29266a11ba424e632fee56d068e0f9ab7a0405c6
Alan <placebo_al@hotmail.com>
2016-10-13 13:10:35 +1100

    add new templates for editing posts, change forms and html, add some very basic security

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.page_list, name='page_list'),
+    url(r'^page/new/$', views.page_new, name='page_new'),
+    url(r'^page/(?P<pk>\d+)/edit/$', views.page_edit, name='page_edit'),
     url(r'^page/(?P<pk>\d+)/$', views.page_detail, name='page_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
-from .models import Page 
+from .models import Page
+from .forms import PageForm
 
 
 def page_list(request):
@@ -11,5 +12,34 @@ def page_list(request):
 def page_detail(request, pk):
 	page = get_object_or_404(Page, pk=pk)
 	return render(request, 'blog/page_detail.html', {'page': page})
+
+
+def page_new(request):
+	if request.method == "POST":
+		form = PageForm(request.POST)
+		if form.is_valid():
+			page = form.save(commit=False)
+			page.author = request.user
+			page.published_date = timezone.now()
+			page.save()
+			return redirect('page_detail', pk=page.pk)
+	else:
+		form = PageForm()
+	return render(request, 'blog/page_edit.html', {'form': form})
+
+
+def page_edit(request, pk):
+    page = get_object_or_404(Page, pk=pk)
+    if request.method == "POST":
+        form = PageForm(request.POST, instance=page)
+        if form.is_valid():
+            page = form.save(commit=False)
+            page.author = request.user
+            page.published_date = timezone.now()
+            page.save()
+            return redirect('page_detail', pk=page.pk)
+    else:
+        form = PageForm(instance=page)
+    return render(request, 'blog/page_edit.html', {'form': form})
 
 
